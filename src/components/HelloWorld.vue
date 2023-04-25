19c5688764cfe254076f1ec7ca7a0dc7b5412093
wouter-adriaens <wouter.adriaens@continuum.be>
2023-04-25 13:17:51 +0200

    #1 Add branding, fonts and webuniversum

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,24 @@
 <script setup lang="ts">
+import { VlButton } from '@govflanders/vl-ui-design-system-vue3';
+
 defineProps<{
-  msg: string
-}>()
+  msg: string;
+}>();
 </script>
 
 <template>
+  <VlButton class="vl-u-spacer">Test button</VlButton>
+
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> + <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
     </h3>
   </div>
 </template>
 
-<style scoped>
+<style lang="scss">
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,17 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/scss/main.scss";`,
+      }
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
-  }
+  },
 })
