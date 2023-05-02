102e49c59b77f9f99602625aa4cc30b93612724f
wouter-adriaens <wouter.adriaens@continuum.be>
2023-05-02 13:31:14 +0200

    #1 Update eslint for typescript imports

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,12 +6,23 @@ module.exports = {
     // add more generic rulesets here, such as:
     // 'eslint:recommended',
     'plugin:vue/vue3-recommended',
-    "@vue/typescript/recommended",
+    '@vue/typescript/recommended',
     // 'plugin:vue/recommended' // Use this if you are using Vue.js 2.x.
   ],
-  "plugins": ["@typescript-eslint", "prettier"],
+  plugins: ['@typescript-eslint', 'prettier', 'import'],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
+    'import/no-unresolved': 'error',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
   },
 };
