69c6a8a33a627bbbd7125ac084468d54be0badb3
wouter-adriaens <wouter.adriaens@continuum.be>
2023-05-26 11:38:09 +0200

    #17 Add header component

---
+++ src/stories/dumb-components/header.stories.ts
@@ -0,0 +1,21 @@
+import OeHeader from '@components/dumb/OeHeader.vue';
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import '@/scss/main.scss';
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+const meta: Meta<typeof OeHeader> = {
+  title: 'Dumb components/Header',
+  component: OeHeader,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof OeHeader>;
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/vue/api/csf
+ * to learn how to use render functions.
+ */
+export const Default: Story = {};
