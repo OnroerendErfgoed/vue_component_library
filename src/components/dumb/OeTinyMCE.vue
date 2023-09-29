ef3aad4ffcfeb530bb2eb6b0b97c84af6004f68d
Axel Verstappen <axel.verstappen@gmail.com>
2023-09-29 11:32:57 +0200

    #114 added all bindable properties

--- src/components/dumb/OeTinyMCE.vue
+++ src/components/dumb/OeTinyMCE.vue
@@ -1,37 +1,48 @@
 <template>
-  <div>
-    <editor
-      v-model="editorValue"
-      api-key="no-api-key"
-      :init="{
-        height: 500,
-        menubar: false,
-        plugins: [
-          'advlist autolink lists link image charmap print preview anchor',
-          'searchreplace visualblocks code fullscreen',
-          'insertdatetime media table paste code help wordcount',
-        ],
-        toolbar:
-          'undo redo | formatselect | bold italic backcolor | \
-            alignleft aligncenter alignright alignjustify | \
-            bullist numlist outdent indent | removeformat | help',
-      }"
-      model-events="change keydown blur focus paste"
-      @change="updateValue"
-    />
-  </div>
+  <editor
+    v-model="editorValue"
+    api-key="no-api-key"
+    :disabled="props.disabled"
+    :init="{
+      height: props.height,
+      menubar: props.menubar,
+      plugins: props.plugins,
+      toolbar: props.toolbar,
+    }"
+    :model-events="modelEvents"
+    @change="updateValue"
+  />
 </template>
 
 <script setup lang="ts">
 import Editor from '@tinymce/tinymce-vue';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 
 interface IOETinyMCEProps {
   value: string;
+  disabled?: boolean;
+  height?: number;
+  menubar?: boolean;
+  plugins?: string | string[];
+  toolbar?: string;
+  modelEvents?: string;
 }
 
 const props = withDefaults(defineProps<IOETinyMCEProps>(), {
   value: '',
+  disabled: false,
+  height: 500,
+  menubar: false,
+  plugins: () => [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table paste code help wordcount',
+  ],
+  toolbar:
+    'undo redo | formatselect | bold italic backcolor | \
+          alignleft aligncenter alignright alignjustify | \
+          bullist numlist outdent indent | removeformat | help',
+  modelEvents: 'change keydown blur focus paste',
 });
 const editorValue = ref(props.value);
 const emit = defineEmits(['update:value']);
@@ -39,6 +50,13 @@ const emit = defineEmits(['update:value']);
 const updateValue = () => {
   emit('update:value', editorValue.value);
 };
+
+watch(
+  () => props.value,
+  () => {
+    editorValue.value = props.value;
+  }
+);
 </script>
 
 <style lang="scss"></style>
