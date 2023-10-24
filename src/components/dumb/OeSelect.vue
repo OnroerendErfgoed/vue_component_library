86c1c3467ab4936a92a95dfdd89b9ca385be03d9
Yannick Kuypers <kuypers.y@gmail.com>
2023-10-24 15:16:51 +0200

    #139 hide results when clicking outside

--- src/components/dumb/OeSelect.vue
+++ src/components/dumb/OeSelect.vue
@@ -1,18 +1,31 @@
 <template>
-  <!-- <label for="vl-select-select-3" class="vl-form__label">
-    Kies een stad of land
-  </label> -->
-  <div @click="showResults = !showResults" class="js-vl-select" role="listbox" data-type="select-one" tabindex="0"
-    dir="ltr">
+  <div
+    v-click-outside="hideResults"
+    @click="showResults = !showResults"
+    class="js-vl-select"
+    role="listbox"
+    data-type="select-one"
+    tabindex="0"
+    dir="ltr"
+  >
     <div class="vl-select__inner">
-      <select name="vl-select-select-3" id="vl-select-select-3"
-        class="vl-select vl-select--block vl-input-field is-hidden" tabindex="-1" style="display:none;"
-        data-choice="active">
+      <select
+        name="vl-select-select-3"
+        id="vl-select-select-3"
+        class="vl-select vl-select--block vl-input-field is-hidden"
+        tabindex="-1"
+        style="display: none"
+        data-choice="active"
+      >
         <option :value="selectedOption.value" selected>{{ selectedOption.label }}</option>
       </select>
       <div class="vl-input-field">
-        <div class="vl-select__item vl-select__item--selectable" data-item="" :data-id="selectedOption.value"
-          :data-value="selectedOption.value">
+        <div
+          class="vl-select__item vl-select__item--selectable"
+          data-item=""
+          :data-id="selectedOption.value"
+          :data-value="selectedOption.value"
+        >
           {{ selectedOption.label }}
         </div>
       </div>
@@ -20,10 +33,18 @@
     <div v-if="showResults" class="vl-select__list vl-select__list--dropdown">
       <div class="vl-select__list" dir="ltr" role="listbox">
         <template v-for="option in options">
-          <div class="vl-select__item vl-select__item--choice vl-select__item--selectable"
-            :class="{ 'is-highlighted': option.selected, '': !option.selected }" data-select-text="Press to select"
-            data-choice="" :data-id="option.value" :data-value="option.value" data-choice-selectable="" :id="option.value"
-            role="treeitem" @click="selectOption(option)">
+          <div
+            class="vl-select__item vl-select__item--choice vl-select__item--selectable"
+            :class="{ 'is-highlighted': option.selected, '': !option.selected }"
+            data-select-text="Press to select"
+            data-choice=""
+            :data-id="option.value"
+            :data-value="option.value"
+            data-choice-selectable=""
+            :id="option.value"
+            role="treeitem"
+            @click="selectOption(option)"
+          >
             <div>
               {{ option.label }}
             </div>
@@ -34,8 +55,9 @@
   </div>
 </template>
 <script lang="ts" setup>
-import type { ISelectOption } from '@models/select';
 import { ref } from 'vue';
+import { vClickOutside } from '@directives/click-outside.directive';
+import type { ISelectOption } from '@models/select';
 
 const showResults = ref<boolean>(false);
 
@@ -43,20 +65,22 @@ const options = [
   { label: 'België', value: 'België', selected: true },
   { label: 'Frankrijk', value: 'Frankrijk', selected: false },
   {
-    label: 'Duitsland - Land in Centraal-Europa. Het heeft een grondgebied van 357.022 km² en grenst in het noorden aan de Oostzee.',
-    value: 'Duitsland', selected: false
-  }
-
+    label:
+      'Duitsland - Land in Centraal-Europa. Het heeft een grondgebied van 357.022 km² en grenst in het noorden aan de Oostzee.',
+    value: 'Duitsland',
+    selected: false,
+  },
 ] as ISelectOption[];
 
 const selectedOption = ref<ISelectOption>(options.find((option) => option.selected) as ISelectOption);
 
 const selectOption = (option: ISelectOption) => {
   selectedOption.value = option;
-  options.forEach(option => option.selected = false);
+  options.forEach((option) => (option.selected = false));
   selectedOption.value.selected = true;
+};
 
-}
+const hideResults = () => (showResults.value = false);
 </script>
 
 <style lang="scss" scoped>
