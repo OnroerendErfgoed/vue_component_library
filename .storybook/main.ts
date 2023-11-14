30286cdf7dd36b418dd9768f30e9e2819ffa39d8
yannickkuypers <kuypers.y@gmail.com>
2023-11-14 15:58:14 +0100

    #91 update storybook (#161)
    * #91 update storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,7 +2,11 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions'
+  ],
   framework: {
     name: '@storybook/vue3-vite',
     options: {
