526acf536a7e5c1071be1380bef8be85b6c9f1a4
wouter-adriaens <wouter.adriaens@continuum.be>
2023-11-20 13:21:57 +0100

    #164 Emit wizard step changed event

--- src/components/dumb/OeWizard.vue
+++ src/components/dumb/OeWizard.vue
@@ -58,7 +58,7 @@
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { VlBadge, VlButton } from '@govflanders/vl-ui-design-system-vue3';
 import { computedAsync } from '@vueuse/core';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import type { IWizardProps } from '@models/wizard';
 
 // Next line should be activated once VlUTooltip is properly exported
@@ -70,7 +70,7 @@ const props = withDefaults(defineProps<IWizardProps>(), {
   allowBarNavigation: false,
   disableSubmitWhenInvalid: false,
 });
-const emit = defineEmits(['submit']);
+const emit = defineEmits(['step-changed', 'submit']);
 
 const currentStep = ref(0);
 const totalSteps = ref(props.steps.length);
@@ -118,6 +118,11 @@ const previousStepsAreValid = async (step: number) => {
 };
 
 const reset = () => (currentStep.value = 0);
+
+watch(
+  () => currentStep.value,
+  () => emit('step-changed', currentStep.value)
+);
 
 defineExpose({ reset });
 </script>
