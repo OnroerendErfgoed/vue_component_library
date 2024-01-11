88a0ab086ba1afd5a557d80d4deb0fc011a0a2ac
wouter-adriaens <wouter.adriaens@continuum.be>
2024-01-11 17:34:56 +0100

    #105 Add no rows overlay component

---
+++ src/components/dumb/NoRowsOverlay.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="ag-overlay-loading-center">
+    <font-awesome-icon :icon="['fas', 'circle-exclamation']" />
+    {{ props.params.noRowsMessage }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+const props = defineProps<{ params: { noRowsMessage: string } }>();
+</script>
