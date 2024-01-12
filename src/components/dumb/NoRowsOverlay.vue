0693fd0c3d29b3d1d0872b498dea87de2fdf15d7
wouter-adriaens <wouter.adriaens@continuum.be>
2024-01-12 11:47:39 +0100

    #105 Add unit tests

--- src/components/dumb/NoRowsOverlay.vue
+++ src/components/dumb/NoRowsOverlay.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="ag-overlay-loading-center">
-    <font-awesome-icon :icon="['fas', 'circle-exclamation']" />
+  <div data-cy="ag-overlay" class="ag-overlay-loading-center">
+    <font-awesome-icon data-cy="warning-icon" :icon="['fas', 'circle-exclamation']" />
     {{ props.params.noRowsMessage }}
   </div>
 </template>
@@ -8,5 +8,5 @@
 <script setup lang="ts">
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-const props = defineProps<{ params: { noRowsMessage: string } }>();
+const props = defineProps<{ params: { noRowsMessage?: string } }>();
 </script>
