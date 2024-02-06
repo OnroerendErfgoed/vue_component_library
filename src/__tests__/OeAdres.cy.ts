2e1043dbd81070cae027cafa729fd5ee8a229b11
wouter-adriaens <wouter.adriaens@continuum.be>
2024-02-06 17:18:07 +0100

    #208 More adres tests fixes

--- src/__tests__/OeAdres.cy.ts
+++ src/__tests__/OeAdres.cy.ts
@@ -655,6 +655,7 @@ const fillInOeAdresBelgium = () => {
 
   // Busnummer selection
   setAutocompleteValue('busnummer', '010');
+  cy.wait(100);
   getAutocomplete('busnummer').should('have.value', '0101');
 };
 
