22283a794f67540927f6c59c2a6e815d755fa9a2
pegahmoh <mohtadi.pegah@gmail.com>
2025-06-15 11:58:42 +0200

    include groep actoren in filter AOE actor autocomplete

--- src/components/smart/FilterAOEActor.vue
+++ src/components/smart/FilterAOEActor.vue
@@ -21,6 +21,7 @@ const props = withDefaults(defineProps<IFilterActorProps>(), {
   api: '',
   value: '',
   getSsoToken: undefined,
+  groepActoren: () => [],
 });
 const emit = defineEmits(['update:value']);
 
@@ -28,6 +29,17 @@ const actorService = new ActorService(props.api, props.getSsoToken);
 
 const actoren = ref<IActor[]>([]);
 const actorValue = computed(() => {
+  if (props.groepActoren.length > 0) {
+    const group = props.groepActoren?.find((g) => g.id === props.value || g.omschrijving === props.value);
+    if (group) {
+      const autocompleteOption: IAutocompleteOption = {
+        title: group.omschrijving,
+        value: group,
+      };
+      return autocompleteOption;
+    }
+  }
+
   const actor = actoren?.value.find((g) => g.uri === props.value || g.omschrijving === props.value);
   const autocompleteOption: IAutocompleteOption = {
     title: actor?.omschrijving as string,
@@ -37,18 +49,35 @@ const actorValue = computed(() => {
 });
 
 const updateValue = (value: IActor) => emit('update:value', value);
+const filterGroepActoren = (searchTerm: string): IAutocompleteOption[] => {
+  return props.groepActoren
+    .filter((group) => group.omschrijving.toLowerCase().includes(searchTerm.toLowerCase()))
+    .map((group) => ({
+      title: group.omschrijving,
+      value: group,
+    }));
+};
 const performAutocompleteSearch = async (searchTerm: string): Promise<IAutocompleteOption[]> => {
   try {
+    let filteredGroups: IAutocompleteOption[] = [];
+
+    if (props.groepActoren.length > 0) {
+      filteredGroups = filterGroepActoren(searchTerm);
+    }
+
     actoren.value = await actorService.getAOEActoren(searchTerm);
-    return actoren.value.map((actor) => {
-      return {
-        title: actor.omschrijving,
-        value: actor.uri,
-      };
-    });
+    const actorOptions = actoren.value.map((actor) => ({
+      title: actor.omschrijving,
+      value: actor.uri,
+    }));
+
+    return [...filteredGroups, ...actorOptions];
   } catch (error) {
     console.error('Error fetching autocomplete data:', error);
-    return Promise.resolve([]);
+    if (props.groepActoren.length > 0) {
+      return filterGroepActoren(searchTerm);
+    }
+    return [];
   }
 };
 </script>
