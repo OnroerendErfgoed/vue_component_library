e2d7de739a007f927b17376a7bfc6fa72cf0097c
wouter-adriaens <wouter@wauwit.be>
2025-09-23 14:13:19 +0200

    #460 Update actor widget

--- src/stories/smart-components/oe-actor-widget.stories.ts
+++ src/stories/smart-components/oe-actor-widget.stories.ts
@@ -14,6 +14,10 @@ const meta: Meta<typeof OeActorWidget> = {
       description: {
         component: `Modal to find and select an actor.`,
       },
+      story: {
+        inline: false,
+        iframeHeight: 800,
+      },
     },
   },
   tags: ['autodocs'],
