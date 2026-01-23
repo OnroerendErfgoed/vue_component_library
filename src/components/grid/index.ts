9973a84e16f8902e97f2a15692b3cdc9ece0ef2c
wouter-adriaens <wouter@wauwit.be>
2026-01-23 16:12:11 +0100

    Fix broken grid workflow export

--- src/components/grid/index.ts
+++ src/components/grid/index.ts
@@ -1,5 +1,5 @@
 // Dumb components
-export { default as OeGridOverflow } from './dumb/OeGridWorkflow.vue';
+export { default as OeGridWorkflow } from './dumb/OeGridWorkflow.vue';
 export { default as OeNoRowsOverlay } from './dumb/OeGridNoRowsOverlay.vue';
 export { default as OeGrid } from './dumb/OeGrid.vue';
 
