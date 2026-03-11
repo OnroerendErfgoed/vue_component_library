782749fa52c81fdfdb8d614c9a46810d5ae8b5c1
wouter-adriaens <wouter@wauwit.be>
2026-03-11 13:23:31 +0100

    #524 Autoresize textarea directive fix collapsing textarea

--- src/directives/auto-resize-textarea.directive.ts
+++ src/directives/auto-resize-textarea.directive.ts
@@ -6,6 +6,7 @@ export const vAutoResizeTextarea = {
       el.style.resize = 'none';
       el.style.height = 'auto';
       el.style.height = `${el.scrollHeight}px`;
+      el.style.minHeight = '58px';
     };
 
     useEventListener(el, 'input', adjustHeight);
