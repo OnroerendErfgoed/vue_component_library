7f467992e5a9035968c372f3be70c8c474cea675
Axel Verstappen <axel.verstappen@gmail.com>
2026-03-30 16:37:26 +0200

    #531 added OePickList component

---
+++ src/components/forms/dumb/OePickList.vue
@@ -0,0 +1,69 @@
+<template>
+  <template v-if="isEditMode">
+    <slot name="input"></slot>
+  </template>
+  <ul :class="['pick-list', isEditMode ? 'vl-u-spacer-top--small divider' : '']">
+    <li v-for="(item, index) in selectedItems" :key="index">
+      {{ itemText(item) }}
+      <VlButton
+        v-if="isEditMode && !disabled"
+        :title="`Verwijder ${itemLabel}`"
+        mod-naked
+        :aria-label="`Verwijder ${itemLabel}`"
+        @click="emit('unselect', item)"
+      >
+        <FontAwesomeIcon :icon="faTrashCan" aria-hidden="true" />
+      </VlButton>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts" generic="T">
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { VlButton } from '@govflanders/vl-ui-design-system-vue3';
+
+withDefaults(
+  defineProps<{
+    selectId: string;
+    selectedItems: T[];
+    placeholderText: string;
+    itemLabel: string;
+    isEditMode: boolean;
+    disabled?: boolean;
+    itemText: (item: T) => string;
+  }>(),
+  {
+    canDelete: true,
+    disabled: false,
+    errors: () => [],
+  }
+);
+
+const emit = defineEmits<{
+  unselect: [item: T];
+}>();
+</script>
+
+<style lang="scss" scoped>
+.pick-list {
+  max-height: 16em;
+  overflow-y: auto;
+
+  &.divider > li {
+    border-bottom: 1px dashed #d5d5d5;
+  }
+
+  > li {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    .vl-button {
+      padding: 0;
+      outline: none;
+      min-height: 0;
+    }
+  }
+}
+</style>
