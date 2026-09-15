8db528059ad92827e933f16c1899f2784b1f9e6b
Axel Verstappen <axel.verstappen@gmail.com>
2026-09-15 16:06:39 +0200

    #198 remove obsolete vite copy

--- vite.config.ts
+++ vite.config.ts
@@ -114,7 +114,6 @@ export default defineConfig({
       targets: [
         { src: 'src/scss/*', dest: 'scss', rename: { stripBase: true } },
         { src: 'src/assets/*', dest: 'assets' },
-        { src: 'public/prettier.config.js', dest: '.' },
       ],
     }),
   ],
